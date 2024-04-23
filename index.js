bc7093e61b31e0ecbe826d389f73c96ddd359d79
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-23 05:29:52 -0500

    Add Rainbow Mode
    Add Rainbow Mode toggle button to toggle draw color from gray
    to random color, per Odin Project extra credit.

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 'use strict';
 
 //Style Terms:
-const KNOB_BG_COLOR = 'whitesmoke';
-const KNOB_BORDER_RADIUS = '100%';
-const BORDER_SIZE = '3px inset rgb(0 0 0)';
-const KNOB_BORDER_SIZE = '8px inset rgb(225 225 225 / 80%)';
-const KNOB_MARGIN = '1%';
-const KNOB_BIG_SIZE = '13rem';
-const KNOB_REDUCED_SIZE = '8rem';
-const KNOB_SIZE = '13rem';
-
 const BANNER_FONT_COLOR = 'rgb(99% 86% 36%)';
 const BANNER_FONT_FAMILY = `'Jersey 25', sans-serif`;
 const BANNER_FONT_SIZE = '4.8rem';
 const BANNER_FONT_STYLE = 'italic';
 const BANNER_TEXT_SHADOW = '1px 1px 1px rgb(0 0 0)';
+const BORDER_SIZE = '3px inset rgb(0 0 0)';
+const DRAW_COLOR = 'rgb(100 100 100 / 25%)';
+const KNOB_BG_COLOR = 'whitesmoke';
+const KNOB_BORDER_RADIUS = '100%';
+const KNOB_BORDER_SIZE = '8px inset rgb(225 225 225 / 80%)';
+const KNOB_MARGIN = '1%';
+const KNOB_REDUCED_SIZE = '8rem';
+const KNOB_SIZE = '13rem';
+const ORIGINAL_RAINBOW_BUTTON = 'rgb(0 0 0 / 80%)';
 
-const docBody = document.querySelector('body');
-docBody.style.backgroundImage = `url('./images/bg.jpg')`;
+let rainbowMode = 'false';
 
 const container = document.querySelector('#container');
-container.style.backgroundColor = 'rgb(255 0 0 / 80%)';
+container.style.backgroundColor = 'rgb(230 0 0)';
 container.style.border = BORDER_SIZE;
 container.style.borderRadius = '1%';
 container.style.boxShadow = '1px 1px 1px 1px rgb(0 0 0)';
 container.style.display = 'flex';
+container.style.flexFlow = 'column nowrap';
 container.style.height = '98vh';
 container.style.width = '100%';
-
-const sketchContainer = document.createElement('div');
-sketchContainer.id = 'sketch-container';
-sketchContainer.style.display = 'flex';
-sketchContainer.style.flexFlow = 'column nowrap';
-sketchContainer.style.height = '100%';
-sketchContainer.style.textAlign = 'center';
-sketchContainer.style.width = '100%';
 
 const bannerDiv = document.createElement('div');
 bannerDiv.id = 'banner';
@@ -46,16 +38,15 @@ bannerDiv.style.margin = 'auto';
 bannerDiv.style.width = '80%';
 
 const magicText = document.createElement('h3');
-magicText.textContent = 'MAGIC';
-magicText.style.background = '#FFD70E';
 magicText.style.background = 'linear-gradient(to bottom right, rgb(255, 215, 14) 0%, rgb(255 255 255 / 90%) 100%)';
-magicText.style.webkitBackgroundClip = 'text';
-magicText.style.webkitTextFillColor = 'transparent';
 magicText.style.fontFamily = BANNER_FONT_FAMILY;
 magicText.style.fontSize = BANNER_FONT_SIZE;
 magicText.style.fontStyle = BANNER_FONT_STYLE;
 magicText.style.fontWeight = '400';
+magicText.textContent = 'MAGIC';
 magicText.style.textShadow = BANNER_TEXT_SHADOW;
+magicText.style.webkitBackgroundClip = 'text';
+magicText.style.webkitTextFillColor = 'transparent';
 
 const etchBanner = document.createElement('h1');
 etchBanner.textContent = 'Odin-Etch';
@@ -84,8 +75,20 @@ screenText.webkitTextFillColor = 'transparent';
 bannerDiv.append(magicText, etchBanner, screenText);
 
 let divQty = 16 * 16;
-
 let gridContainer = generateDivGrid(divQty);
+styleGrid(gridContainer);
+
+const rainbowGridsButton = document.createElement('button');
+rainbowGridsButton.addEventListener('click', toggleRainbowMode);
+rainbowGridsButton.id = 'rainbow-button';
+rainbowGridsButton.style.alignSelf = 'center';
+rainbowGridsButton.style.backgroundColor = ORIGINAL_RAINBOW_BUTTON;
+rainbowGridsButton.style.border = '3px outset whitesmoke';
+rainbowGridsButton.style.borderRadius = '100%';
+rainbowGridsButton.style.color = 'whitesmoke';
+rainbowGridsButton.style.height = '8vh';
+rainbowGridsButton.style.width = '15%';
+rainbowGridsButton.textContent = 'Rainbow Mode';
 
 const knobsDiv = document.createElement('div');
 knobsDiv.id = 'knobs-div';
@@ -95,8 +98,8 @@ knobsDiv.style.justifyContent = 'space-between';
 knobsDiv.style.padding = '1% auto';
 
 const leftKnob = document.createElement('div');
-leftKnob.className = 'knobs';
 const rightKnob = document.createElement('div');
+leftKnob.className = 'knobs';
 rightKnob.className = 'knobs';
 
 let knobArr = [leftKnob, rightKnob];
@@ -111,8 +114,8 @@ knobArr.forEach(knob => {
         knob.style.width = KNOB_SIZE;
     }
     else if (divQty === 16) {
-        knob.style.height = KNOB_BIG_SIZE;
-        knob.style.width = KNOB_BIG_SIZE;
+        knob.style.height = KNOB_SIZE;
+        knob.style.width = KNOB_SIZE;
     }
     else {
         knob.style.height = KNOB_REDUCED_SIZE;
@@ -120,15 +123,13 @@ knobArr.forEach(knob => {
     }
 });
 
-knobArr = null;
-
 const sixteenButton = document.createElement('button');
 const thirtyTwoButton = document.createElement('button');
 const sixtyFourButton = document.createElement('button');
 
-sixteenButton.textContent = '16x16';
-thirtyTwoButton.textContent = '32x32';
-sixtyFourButton.textContent = '64x64';
+sixteenButton.textContent = '16 x 16';
+thirtyTwoButton.textContent = '32 x 32';
+sixtyFourButton.textContent = '64 x 64';
 
 sixteenButton.addEventListener('click', clickHandler);
 thirtyTwoButton.addEventListener('click', clickHandler);
@@ -136,6 +137,7 @@ sixtyFourButton.addEventListener('click', clickHandler);
 
 let buttonArr = [sixteenButton, thirtyTwoButton, sixtyFourButton];
 buttonArr.forEach(button => {
+    button.className = 'options';
     button.style.border = BORDER_SIZE;
     button.style.borderRadius = '10%';
     button.style.boxShadow = '3px 3px 3px 3px rgb(0 0 0)';
@@ -145,56 +147,45 @@ buttonArr.forEach(button => {
 });
 
 const buttonDiv = document.createElement('div');
+buttonDiv.id = 'button-div';
 buttonDiv.style.alignItems = 'center';
 buttonDiv.style.display = 'flex';
 buttonDiv.style.gap = '5%';
 
 buttonDiv.append(sixteenButton, thirtyTwoButton, sixtyFourButton);
-
 knobsDiv.append(leftKnob, buttonDiv, rightKnob);
-
-sketchContainer.append(bannerDiv, gridContainer, knobsDiv);
-container.appendChild(sketchContainer);
-
+container.append(bannerDiv, gridContainer, rainbowGridsButton, knobsDiv);
 
 /*-------------------------------------------------------*/
 
 function clickHandler(e) {
     new Audio('./click.mp3').play();
-    let grid;
+    const cancelChange = `Good choice, it's a masterpiece! ^_^`;
+    const confirmChange = 'Are you sure? Everything etched will be lost.';
+    
     switch(e.target.textContent) {
-        case '64x64':
-            console.log('64 clicked!');
-            grid = generateDivGrid(64 * 64);
+        case '64 x 64':
+            console.log('Size 64 x 64 clicked!');
+            confirm(confirmChange) ? resetGrid(64 * 64) : alert(cancelChange);
             break;
-        case '32x32':
-            console.log('32 clicked!');
-            grid = generateDivGrid(32 * 32);
+            case '32 x 32':
+                console.log('Size 32 x 32 clicked!');
+                confirm(confirmChange) ? resetGrid(32 * 32) : alert(cancelChange);
             break;
         default:
-            console.log('16 clicked!');
-            grid = generateDivGrid(16 * 16);
-            break;
+            console.log('Size 16 x 16 clicked!');
+            confirm(confirmChange) ? resetGrid(16 * 16) : alert(cancelChange);
     }
-    grid.className = 'grid-container';
-    grid.style.alignItems = 'center';
-    grid.style.border = BORDER_SIZE;
-    grid.style.borderRadius = '1%';
-    grid.style.display = 'flex';
-    grid.style.flexFlow = 'row wrap';
-    grid.style.justifyContent = 'center';
-    grid.style.margin = '0 auto 2%';
-    grid.style.height = '80%';
-    grid.style.width = '80%';
-                
-    let oldGrid = document.querySelector('.grid-container');  
-    sketchContainer.replaceChild(grid, oldGrid);
 }
-            
+
 function drawOnGrid(e) {
-    e.target.style.backgroundColor = 'rgb(0 0 0 / 95%)';
+    if (rainbowMode === true) {
+        e.target.style.backgroundColor = getRandomColor();
+    } else {
+        e.target.style.backgroundColor = DRAW_COLOR;
+    }
 }
-            
+
 function generateDivGrid(qty) {
     let divContainer = document.createElement('div');
     divContainer.className = 'grid-container';
@@ -204,34 +195,91 @@ function generateDivGrid(qty) {
     divContainer.style.display = 'flex';
     divContainer.style.flexFlow = 'row wrap';
     divContainer.style.justifyContent = 'center';
-    divContainer.style.margin = '0 auto 2%';
-    divContainer.style.height = '80%';
+    divContainer.style.margin = '5% auto 2%';
+    divContainer.style.height = '90%';
     divContainer.style.width = '80%';
+    
+    let divTemplate = document.createElement('div');
+    divTemplate.style.backgroundColor = 'rgb(200 200 200 / 80%)';
+    divTemplate.id = 'div-template';
+    divTemplate.style.flex = '1 1 auto';
+    
     for (let i = 0; i < qty; i++) {
-        let tempDiv = document.createElement('div');
-        tempDiv.style.backgroundColor = 'rgb(248 248 248 / 96%)';
+        let tempDiv = divTemplate.cloneNode();
         tempDiv.className = 'grid-div';
-        tempDiv.style.border = '1px solid rgb(0 0 0 / 80%)';
-        tempDiv.style.flex = '1 1 auto';
+        tempDiv.id = Math.trunc(Math.random() * 100000);
         //16*16:
         if (qty <= 256) {
-            tempDiv.style.height = '6%';
+            tempDiv.style.border = '5px outset rgb(0 0 0 / 15%)';
+            tempDiv.style.height = '6.2%';
             tempDiv.style.width = '6%';
         } 
         //32*32:
         else if (qty > 256 && qty <= 1024) {
-            tempDiv.style.height = '3%';
+            tempDiv.style.border = '2px solid rgb(0 0 0 / 15%)';
+            tempDiv.style.height = '3.13%';
             tempDiv.style.width = '3.1%';
         } 
         //64*64:
         else {
-            tempDiv.style.border = '0.5px solid rgb(0 0 0 / 80%)';
+            tempDiv.style.border = '1.45px solid rgb(0 0 0 / 25%)';
             tempDiv.style.height = '1.14%';
             tempDiv.style.width = '1.55%';
         }
-
+        
         tempDiv.addEventListener('mouseover', drawOnGrid);
         divContainer.appendChild(tempDiv);
     }
     return divContainer;
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    let i = 0;
+    do {
+        color += letters[Math.floor(Math.random() * 16)];
+        i++;
+    } while (i < 6);
+    color = hexToRgb(color);
+    return color;
+}
+
+function hexToRgb(hexNum) {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexNum);
+    let r = parseInt(result[1], 16);
+    let g = parseInt(result[2], 16);
+    let b = parseInt(result[3], 16);
+    let randomOpacity = Math.floor(Math.random() * 100);
+    return `rgb(${r} ${g} ${b} / ${randomOpacity}%)`;
+}
+
+function resetGrid(gridSize) {
+    let newGrid = generateDivGrid(gridSize);
+    styleGrid(newGrid);
+    let oldGrid = document.querySelector('.grid-container');
+    container.replaceChild(newGrid, oldGrid);
+}
+
+function styleGrid(grid) {
+    grid.className = 'grid-container';
+    grid.style.alignItems = 'center';
+    grid.style.backgroundColor = 'rgb(150 150 150)';
+    grid.style.border = BORDER_SIZE;
+    grid.style.borderRadius = '1%';
+    grid.style.display = 'flex';
+    grid.style.flexFlow = 'row wrap';
+    grid.style.justifyContent = 'center';
+    grid.style.margin = '0 auto 2%';
+    grid.style.height = '80%';
+    grid.style.width = '80%';
+}
+
+function toggleRainbowMode() {
+    rainbowMode = !rainbowMode;
+    if (rainbowMode) {
+        rainbowGridsButton.style.backgroundColor = getRandomColor();
+    } else {
+        rainbowGridsButton.style.backgroundColor = ORIGINAL_RAINBOW_BUTTON;
+    }
 }
